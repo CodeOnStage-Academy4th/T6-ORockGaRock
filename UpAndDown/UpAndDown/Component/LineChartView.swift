d60a2aa5ec80ac1f6a421f18df49be9ad99176cf
Yang Sijun <yang@sijun.dev>
2025-08-09 02:55:02 +0900

    LineChartView 배경색 추가

--- UpAndDown/UpAndDown/Component/LineChartView.swift
+++ UpAndDown/UpAndDown/Component/LineChartView.swift
@@ -8,30 +8,42 @@
 import SwiftUI
 
 struct LineChartView: View {
+    @State var visibleDataCount: Int = 60
     let data: [Double]
     let lineColor: Color
     let lineWidth: CGFloat
+    let bottomPadding: CGFloat
+    
+    init(data: [Double], lineColor: Color, lineWidth: CGFloat, fillArea: Bool = false, fillColor: Color? = nil, bottomPadding: CGFloat = 0.2) {
+        self.data = data
+        self.lineColor = lineColor
+        self.lineWidth = lineWidth
+        self.bottomPadding = bottomPadding
+    }
     
     var body: some View {
-        ScrollViewReader { proxy in
-            ScrollView(.horizontal, showsIndicators: false) {
-                LineShape(dataPoints: data)
-                    .stroke(lineColor, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                    .frame(width: CGFloat(data.count * 10))
-                    .id(0)
-            }
-            .frame(height: 200)
-            .onAppear {
-                proxy.scrollTo(0, anchor: .trailing)
-            }
+        ZStack {
+            // 채우기 영역
+            LineShape(dataPoints: data.suffix(visibleDataCount), fillArea: true, bottomPadding: bottomPadding)
+                .fill(LinearGradient(colors: [lineColor.opacity(0.8), lineColor.opacity(0)], startPoint: .top, endPoint: .bottom))
+            
+            // 선 그리기
+            LineShape(dataPoints: data.suffix(visibleDataCount), fillArea: false, bottomPadding: bottomPadding)
+                .stroke(lineColor, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
         }
+        .frame(maxWidth: .infinity)
+        .padding(4)
     }
 }
 
 #Preview {
-    LineChartView(
-        data: [1, 2, 4, 3, 2, 5, 1, 2, 4, 5],
-        lineColor: Color.orange,
-        lineWidth: 3
-    )
+    VStack(spacing: 20) {
+        // 선만 그리기
+        LineChartView(
+            data: [1, 2, 4, 3, 2, 5, 1, 2, 4, 5],
+            lineColor: Color.orange,
+            lineWidth: 3
+        )
+    }
+    .padding()
 }
